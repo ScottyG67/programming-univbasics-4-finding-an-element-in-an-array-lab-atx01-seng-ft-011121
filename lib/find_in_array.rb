bce14d4ded74f96528ae72bafdb02e3a477ba161
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-21 20:21:59 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,11 @@
+require 'pry'
 def find_element_index(array, value_to_find)
   count = 0
+  binding.pry
   while array < array.length do
+    binding.pry
     if array[count]==value_to_find
+      binding.pry
       return count
     end
       count+=1
