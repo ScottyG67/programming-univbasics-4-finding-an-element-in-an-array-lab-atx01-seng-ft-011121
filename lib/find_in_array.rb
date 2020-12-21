ae64bedaf76046c7a8eb727673c223f1427e11ba
Scott Gloyna <scott.gloyna01@gmail.com>
2020-12-21 20:24:50 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ require 'pry'
 def find_element_index(array, value_to_find)
   count = 0
   #binding.pry
-  while array < array.length do
+  while count < array.length do
     binding.pry
     if array[count]==value_to_find
       binding.pry
